0008bce2e35212b6bb2c33caf157facb4ce8bbb7
michael irvin <michael.w.irvin@vanderbilt.edu>
2014-03-04 00:45:18 -0600

    updated hypothesis

--- anrm/numtools/fit_5_0.py
+++ anrm/numtools/fit_5_0.py
@@ -101,7 +101,7 @@ def step(mcmc):
              mcmc.accept_likelihood, mcmc.accept_prior, mcmc.accept_posterior)
 
 #----Experiment Name--------
-Exp_name = ('CompII_Hypthesis_1')
+Exp_name = ('CompII_Hypthesis_2')
 
 #----Data and conditions----
 ydata = ydata_fn()
