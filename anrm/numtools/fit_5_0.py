31532afb6aa7cd16fe601456c27f2aaccf2c9d1e
michael irvin <michael.w.irvin@vanderbilt.edu>
2014-03-04 00:20:18 -0600

    debugged fit_5_0.py

--- anrm/numtools/fit_5_0.py
+++ anrm/numtools/fit_5_0.py
@@ -171,9 +171,8 @@ for k in conditions.keys():
     
     yfinal = ct.normalize(ct.extract_records(solve.simulate(mcmc.position, observables=True, initial_conc=conditions[k]),ynorm[k][1]), option = 1)
     pickle.dump(yinitial, open('%s_Final_Values_%s.pkl' % (Exp_name, k), 'wb'))
-    ii = ii+1
 
-    pickle.dump(mcmc.position, open('%s_Position' % Exp_name, 'wb'))
+pickle.dump(mcmc.position, open('%s_Position' % Exp_name, 'wb'))
 
 """
 # plot data
